65fe099171a4947a02930b402c6851b17884924f
golovanya <golovanya1507@gmail.com>
2023-09-17 14:17:55 +0300

    check

--- pages/sample/index.js
+++ pages/sample/index.js
@@ -3,7 +3,7 @@ const plain = () => null;
 export const getServerSideProps = async ({ res }) => {
   res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end('\nfunction task(x) {\nreturn x* this.x**2\n} ');
+  res.end('\nfunction task(x) {\nreturn x * this.x**2\n} ');
   return { props: {} };
 }
 
