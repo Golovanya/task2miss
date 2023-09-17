37fd8ec4ed6c8bda31a2fbf2b4f8a0041ffd6a43
golovanya <golovanya1507@gmail.com>
2023-09-17 19:07:33 +0300

    check

--- pages/sample/index.js
+++ pages/sample/index.js
@@ -4,7 +4,7 @@ export const getServerSideProps = async ({ res }) => {
   res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('X-Author', 'golovanya_ ');
-  res.end('\nfunction task(x) {\nreturn x * this.x**2\n} ');
+  res.end('function task(x) {\nreturn x * Math.pow(x,2)\n}');
   return { props: {} };
 }
 
