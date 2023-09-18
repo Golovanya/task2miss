f2eb96af27219818c3d21daf2569670130a9dd5f
golovanya <golovanya1507@gmail.com>
2023-09-18 21:54:29 +0300

    check

--- pages/fetch/index.js
+++ pages/fetch/index.js
@@ -4,7 +4,7 @@ export const getServerSideProps = async ({ res }) => {
   res.setHeader('Content-Type', 'text/htmk; charset=UTF-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('X-Author', 'golovanya_ ');
-  res.end('<!doctype html> <html> <head> </head> <body> <input id="inp"/> <button id="bt" onclick="replaceinputvalue()">нажми меня</button> <script>{ function replaceinputvalue() { const input = document.getelementbyid("inp"); const url = input.value; (async () => { const response = await fetch(url); input.value = await response.text(); })(); } }</script> </body> </html>');
+  res.send('<!doctype html> <html> <head> </head> <body> <input id="inp"/> <button id="bt" onclick="replaceinputvalue()">нажми меня</button> <script>{ function replaceinputvalue() { const input = document.getelementbyid("inp"); const url = input.value; (async () => { const response = await fetch(url); input.value = await response.text(); })(); } }</script> </body> </html>');
   return { props: {} };
 }
 
