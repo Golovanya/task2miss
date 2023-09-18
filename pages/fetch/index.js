9f7d8d990ca86912d7d4b1e83637c21b66ee5226
golovanya <golovanya1507@gmail.com>
2023-09-18 21:45:34 +0300

    check

---
+++ pages/fetch/index.js
@@ -0,0 +1,11 @@
+const plain = () => null;
+
+export const getServerSideProps = async ({ res }) => {
+  res.setHeader('Content-Type', 'text/htmk; charset=UTF-8');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('X-Author', 'golovanya_ ');
+  res.end('<!doctype html> <html> <head> </head> <body> <input id="inp"/> <button id="bt" onclick="replaceinputvalue()">нажми меня</button> <script>{ function replaceinputvalue() { const input = document.getelementbyid("inp"); const url = input.value; (async () => { const response = await fetch(url); input.value = await response.text(); })(); } }</script> </body> </html>');
+  return { props: {} };
+}
+
+export default plain;
